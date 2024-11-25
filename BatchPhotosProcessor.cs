86d335b14c776e5a78852d2d0a1c789176c7d5b7
k-zhouk <konstantin_zhouk@outlook.com>
2024-11-26 00:04:12 +0100

    First commit

--- BatchPhotosProcessor.cs
+++ BatchPhotosProcessor.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.Reflection;
 
 namespace BatchPhotosProcessor
 {
@@ -10,7 +11,9 @@ namespace BatchPhotosProcessor
     {
         static void Main(string[] args)
         {
-
+            // Getting the version of the program
+            string appVerion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            Console.WriteLine($"{Environment.NewLine}******************** Batch Photos Processer (ver. {appVerion}) ********************{Environment.NewLine}");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# BatchPhotosProcessor
+25.11.2024
+
+This is a console application for batch processing of photos, resizing them in particular. Could be useful, if there is a need to resize lots of photos at once, e.g. for a blog.
+Configuration is done with an XML file.
+Specify an input folder, ourput folder and type of resizing ("resize_percentage" or "resize_resolution"). If both the "resize_percentage" and "resize_resolution" are not empty, then the "resize_percentage" is used.
